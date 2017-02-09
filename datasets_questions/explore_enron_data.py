153cdcbda3c180145556abc2fdb7bb8212347b0a
= <=>
2017-02-09 17:11:14 +0530

    done with enron mini project

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -18,11 +18,34 @@
 import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
-
-print len(enron_data)
+num = len(enron_data)
+print num
 print (len(enron_data['SKILLING JEFFREY K']))
-c = 0
+c1 = 0
+c2 = 0
+c3 = 0
+c4 = 0
+name = 'SKILLING JEFFREY K'
 for key in enron_data.keys():
     if enron_data[key]["poi"] == 1:
-        c = c+1
-print c
+        c1 = c1 + 1
+    if (enron_data[key]["salary"] != "NaN"):
+        c2+=1
+    if enron_data[key]["email_address"] != "NaN":
+        c3+=1
+    if (enron_data[key]["total_payments"] == "NaN"):
+        c4+=1
+
+print c1
+
+print enron_data["PRENTICE JAMES"]["total_stock_value"]
+print enron_data["COLWELL WESLEY"]["from_this_person_to_poi"]
+print enron_data["SKILLING JEFFREY K"]["exercised_stock_options"]
+print enron_data["SKILLING JEFFREY K"]["total_payments"]
+print enron_data["LAY KENNETH L"]["total_payments"]
+print enron_data["FASTOW ANDREW S"]["total_payments"]
+print c2
+print c3
+print c4
+per = (c4*100)/num
+print per
